f352c806d8f05b5296a17cbd82007922914da5d4
samyakbhatkar9 <samyakbhatkar9@gmail.com>
2026-07-20 15:10:18 +0000

    A challenge to myself that be the one who you always want to be

--- Second.py
+++ Second.py
@@ -1,1 +1,3 @@
 print("What if I still do it, What if I make recommendation system of Chitrapat so powerfull that no one even think to watch movie from Youtube after that.")
+
+print("Do You Have The Guts To Give Your 100 percent Without Any Hesitation")
