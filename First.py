592a2f5d8bc24e82d969379c40ca671e5b09c48a
samyakbhatkar9 <samyakbhatkar9@gmail.com>
2026-07-21 14:15:57 +0000

    Aab Third File Add Kar Di Gayi Hai

--- First.py
+++ First.py
@@ -2,3 +2,5 @@ print("I had made changes in the code")
 for i in range(4):
     print("I am greatest of all the time")
 print("The End")
+
+print("Practice Makes Man Perfect")
--- Third.py
+++ Third.py
@@ -1,3 +1,4 @@
 # Me hoon KhanNayak
 # Me hoon Don
 # Me hoon Kon 
+# I am Iron-Man
